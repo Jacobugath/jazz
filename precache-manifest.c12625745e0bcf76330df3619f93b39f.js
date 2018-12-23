a2fdb2666af0a84acf48b655190dc999e00162d4
Jacob Morris <jacobthomasmorris@gmail.com>
2018-12-22 22:20:44 -0700

    Updates

---
+++ precache-manifest.c12625745e0bcf76330df3619f93b39f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d11926d1419b1bffb739",
+    "url": "/jazz/static/js/runtime~main.d11926d1.js"
+  },
+  {
+    "revision": "6ec1c2027480f12c918d",
+    "url": "/jazz/static/js/main.6ec1c202.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/jazz/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "6ec1c2027480f12c918d",
+    "url": "/jazz/static/css/main.19574547.chunk.css"
+  },
+  {
+    "revision": "5d9260862bc74600416ccfa972293b0b",
+    "url": "/jazz/index.html"
+  }
+];
